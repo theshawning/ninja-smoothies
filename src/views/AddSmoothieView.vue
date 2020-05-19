14060ba99d12365bbf290d2a7ebc1b9ad51c7148
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-19 06:40:29 +0000

    created addSmoothie component and page view

---
+++ src/views/AddSmoothieView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <AddSmoothie />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import AddSmoothie from '@/components/AddSmoothie.vue'
+
+export default {
+  name: 'AddSmoothieView',
+  components: {
+    Navbar,
+    AddSmoothie
+  }
+}
+</script>
