163b680882de7d5c8d876de3d6c9ef893d0670d9
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-19 09:25:29 +0000

    completed editSmoothie component to allow users to edit smoothies connected component to firestore to update database

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -25,6 +25,7 @@
 
 <script>
 import db from '../firebase/init'
+import slugify from 'slugify'
 
 export default {
   name: 'EditSmoothie',
@@ -37,7 +38,27 @@ export default {
   },
   methods: {
     EditSmoothie(){
-      console.log(this.smoothie.title, this.smoothie.ingredients)
+      if(this.smoothie.title){
+        this.feedback = null
+        // create a slug
+        this.smoothie.slug = slugify(this.smoothie.title, {
+          replacement: '-',
+          remove: /[$*_+~.()?'"!\-:@]/g,
+          lower: true
+        })
+        console.log(this.slug)
+        db.collection('smoothies').doc(this.smoothie.id).update({
+          title: this.smoothie.title,
+          ingredients: this.smoothie.ingredients,
+          slug: this.smoothie.slug
+        }).then(() => {
+          this.$router.push({ name: 'Home' })
+        }).catch(err => {
+          console.log(err)
+        })
+      } else {
+        this.feedback = 'You must enter a smoothie title'
+      }
     },
     addIng(){
       if(this.another){
